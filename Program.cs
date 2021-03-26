2e74a418ba1de33c7df9eefd51cf251d7052a335
Adam-Dodson00 <62917191+Adam-Dodson00@users.noreply.github.com>
2021-03-26 13:02:45 +1100

    complete FizzBuzz

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int fizz = 3;
+            int buzz = 5;
+            int limit = 10000;
+
+            while(count <= limit){
+                string output = "";
+
+                if(count % fizz == 0){
+                    output += "fizz";
+                }
+                if(count % buzz == 0){
+                    output += "buzz";
+                }
+                if(count % buzz != 0 && count % fizz != 0){
+                    output += count;
+                }
+
+                Console.WriteLine(output);
+                count++;
+            }
+        }
+    }
+}
